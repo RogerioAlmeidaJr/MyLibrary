c8d3ee919014be74186962137886a29c8b634b65
RogerioAlmeidaJr <rogeriojunior23112002@gmail.com>
2023-08-29 22:58:31 -0300

    Conexão ao BD e criação do menu superior

---
+++ Conection/conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "webLivraria";
+
+$mysqli = new mysqli($hostname, $username, $password, $database);
+
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- Conexão ao BD -->
+<?php include("Conection/conexao.php")?>
+
+<html>
+<head>
+    <meta charset = "UTF-8">
+    <title>CRUD_Livraria</title>
+    <link rel = "stylesheet" href = "Style/style.css">
+</head>
+<body>
+
+    <!-- Menu Superior -->
+    <nav class = "nav-menu-sup">
+
+        <ul class = "ul-menu-sup">
+            <li><a href = "/">Listar</li>
+            <li><a href = "/">Cadastrar</li>
+            <li><a href = "/">Alterar</li>
+            <li><a href = "/">Remover</li>
+        </ul>
+
+    </nav>
+
+    <!-- Corpo do site -->
+    <nav class = "nav-corpo">
+
+        <a> BEM-VINDO </a>
+
+    </nav>
+
+</body>
+</html>
